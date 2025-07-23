fc4a8d8909816fad4f8239c3bb0e5e1aa6732097
fpastene <felixpastene@gmail.com>
2025-07-23 14:51:21 -0400

    agrega funcion no terminada, solo para guardar v.

--- js/script_dos.js
+++ js/script_dos.js
@@ -1,3 +1,19 @@
+
+document.addEventListener('DOMContentLoaded', function() {
+    // Ejemplo: desplazamiento suave para enlaces de anclaje
+    document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+        anchor.addEventListener('click', function (e) {
+            e.preventDefault();
+
+            document.querySelector(this.getAttribute('href')).scrollIntoView({
+                behavior: 'smooth'
+            });
+        });
+    });
+});
+
+cons
+
 const world = "Mundo";
 let msg = 'Hola';
 console.log(`ya va : ${msg + " " + world}`);
